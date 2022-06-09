a61d53958608d3ceef4bf7eb28650066baab18dd
吳承泰 <94054937+tom71012@users.noreply.github.com>
2022-06-09 17:01:40 +0800

    Update sign up.js

--- homework/html/sign up.js
+++ homework/html/sign up.js
@@ -6,5 +6,5 @@ btn.addEventListener("click",function()
   {
     if(Password.value.length < 8 ) document.write("請設置八位數以上的密碼");
     else if(Password.value != Confirm_Password.value)  document.write("請輸入相同的密碼");
-    else location.href="signin.html";
+    else location.href="sign in.html";
  })
