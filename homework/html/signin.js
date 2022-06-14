f8d805bc24a4cd370bd1b23f9e204b58298c2ca7
吳承泰 <94054937+tom71012@users.noreply.github.com>
2022-06-14 15:50:36 +0800

    Delete signin.js

--- homework/html/signin.js
+++
@@ -1,8 +0,0 @@
-var password = document.getElementById("password");
-var passwordcheck = document.getElementById("passwordcheck");
-
-passwordcheck.addEventListener("click",function()
-{
-   if(password.value.length > 8 ) location.href='autumn.html';
-   else  document.write("請設置八位數以上的密碼");
-})
